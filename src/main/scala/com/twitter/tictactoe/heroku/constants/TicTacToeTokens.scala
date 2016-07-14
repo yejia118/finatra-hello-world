a028b4644deef9f05b99ea52c2df5020b132c9a0
jiay <jiay@twitter.com>
2016-07-14 14:11:56 -0700

    Add test team to authorized team list

--- src/main/scala/com/twitter/tictactoe/heroku/constants/TicTacToeTokens.scala
+++ src/main/scala/com/twitter/tictactoe/heroku/constants/TicTacToeTokens.scala
@@ -1,5 +1,9 @@
 package com.twitter.tictactoe.heroku.constants
 
 object TicTacToeTokens {
-  val authorizedTokens: Set[String] = Set("CyO37xuT10jQSCcegfTWqdFm")
+  val authorizedTokens: Set[String] =
+    Set(
+      "CyO37xuT10jQSCcegfTWqdFm", // Team ilovebanana
+      "Ps8fwahfzzRBpoIuuWqDlqH6" // Team yejia118test0
+    )
 }
